6f7e980f75e657d5e1bf857583c9ec062ef119b2
MahmoudAdelSaleh <mahmoudadelsaleh@outlook.com>
2025-08-23 14:52:47 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, serverTimestamp, doc, updateDoc, deleteDoc, getDoc, writeBatch } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, serverTimestamp, doc, updateDoc, deleteDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+// --- تم تحديث بيانات الاتصال هنا ---
 const firebaseConfig = {
-    apiKey: "AIzaSyAWhNkYnC2n4iuHvdp1O_-S5suxivDidgc",
-    authDomain: "cash-84acc.firebaseapp.com",
-    projectId: "cash-84acc",
-    storageBucket: "cash-84acc.appspot.com",
-    messagingSenderId: "774737678098",
-    appId: "1:774737678098:web:8b918375b4e51692b7edcb",
-    measurementId: "G-GRKNPJ45N1"
+  apiKey: "AIzaSyCfOzC8gkqkVSNZ3frtnnCMxbeq-v7yaTY",
+  authDomain: "almas-store-a51eb.firebaseapp.com",
+  projectId: "almas-store-a51eb",
+  storageBucket: "almas-store-a51eb.appspot.com", // تم تصحيح الخطأ هنا firebasestorage.app -> appspot.com
+  messagingSenderId: "502522968593",
+  appId: "1:502522968593:web:52dc253f8ea12f4d5bcfb0",
+  measurementId: "G-G0WC9BJ6JW"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
+// --- باقي الدوال المساعدة كما هي ---
 export const addDocument = async (collectionName, data) => await addDoc(collection(db, collectionName), { ...data, createdAt: serverTimestamp() });
 export const readCollection = (collectionName, callback, orderByField = "name", orderDirection = "asc") => {
     const q = query(collection(db, collectionName), orderBy(orderByField, orderDirection));
